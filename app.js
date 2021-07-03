6b03402760e867b3ea78e6da0738604f5fdb0537
earlgreybutter <duckduck31216@gmail.com>
2021-07-04 06:04:54 +0900

    sort filter

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
+var cors = require("cors"); // 교차통신 모듈 호출
 
 // [ CONFIGURE mongoose ]
 
@@ -28,8 +29,10 @@ var Book = require("./models/book");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(cors()); // 교차통신 적용
+
 // [CONFIGURE SERVER PORT]
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 3000;
 
 // [CONFIGURE ROUTER]
 var router = require("./routes")(app, Book);
--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,24 @@
 module.exports = function (app, Book) {
   // GET ALL BOOKS
   app.get("/api/books", function (req, res) {
-    // Select the collection on which you want to perform an operation
-    // var butter = req.db.collection('butter');
+    var query = req.query;
+    console.log(query);
 
-    // butter.find({}, {}, function(err, books) {
-    //     if (err) return res.status(500).send({ error: 'database failure' });
-    //     res.json(books);
-    // })
-
-    Book.find(function (err, books) {
-      if (err) return res.status(500).send({ error: "database failure" });
-      res.json(books);
-    });
+    if (!!query.sortdatafield && !!query.sortorder) {
+      var sorting = {
+        sort: [[query.sortdatafield, query.sortorder]],
+      };
+      Book.find()
+        .sort(sorting.sort)
+        .exec(function (err, books) {
+          if (err) return res.status(500).send({ error: "database failure" });
+          res.json(books);
+        });
+    } else {
+      Book.find().exec(function (err, books) {
+        if (err) return res.status(500).send({ error: "database failure" });
+        res.json(books);
+      });
+    }
   });
 };
